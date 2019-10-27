98245b5e8e6206aa31b666756af909f59634aee8
wuhong <wuhong@cloud-star.com.cn>
2019-10-27 08:53:45 +0800

    feat(*): 测试线程池

---
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/common/study/thread/pool/ThreadPoolUseTest.java
@@ -0,0 +1,45 @@
+package cn.com.cloudstar.rightcloud.framework.common.study.thread.pool;
+
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory.Builder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+
+/**
+ * @author Hong.Wu
+ * @date: 10:38 2019/10/15
+ */
+public class ThreadPoolUseTest {
+
+    public static void main(String[] args) {
+//        正例1：
+        //org.apache.commons.lang3.concurrent.BasicThreadFactory
+//        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1,
+//                                                                                   new Builder().namingPattern(
+//                                                                                           "example-schedule-pool-%d")
+//                                                                                                                   .daemon(true)
+//                                                                                                                   .build());
+
+//        正例2：
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNamePrefix("demo-pool-").build();
+
+//Common Thread Pool
+        ExecutorService pool = new ThreadPoolExecutor(5, 200,
+                                                      0L, TimeUnit.MILLISECONDS,
+                                                      new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory,
+                                                      new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < 10; i++) {
+            pool.execute(() -> System.out.println(Thread.currentThread().getName()));
+        }
+
+        pool.shutdown();//gracefully shutdown
+
+
+    }
+}
