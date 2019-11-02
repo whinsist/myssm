850976920129e87ba7ae549f045939055db6175a
wuhong <wuhong@cloud-star.com.cn>
2019-11-03 07:30:20 +0800

    feat(*): 测试-hutool

--- myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/testtreeutil/Test3Hutool.java
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/testtreeutil/Test3Hutool.java
@@ -1,6 +1,9 @@
 package cn.com.cloudstar.rightcloud.framework.testtreeutil;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.List;
@@ -8,12 +11,22 @@ import java.util.List;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
+import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.ObjectId;
 import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.EscapeUtil;
+import cn.hutool.core.util.HashUtil;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
 import cn.hutool.core.util.ZipUtil;
 
 /**
@@ -28,11 +41,108 @@ public class Test3Hutool {
 //        testConvert();
 
 //        testFile();
+//        testZip();
+//        testUuid();
 
 //        testStrUtil();
+//        testIoUtil();
 
-//        testZip();
-        testUuid();
+//        testUrlUtil();
+
+//        testReflectUtil();
+
+//        testRuntimeUtil();
+
+//        testNumberUtil();
+        
+        testRandomUtil();
+    }
+
+    private static void testRandomUtil() {
+        int randomInt = RandomUtil.randomInt(12, 100);
+        RandomUtil.randomUUID();
+        String s = RandomUtil.randomString("12323323ASDFSDFSDF", 5);
+        System.out.println(s);
+
+    }
+
+    private static void testNumberUtil() {
+        double te1 = 123456.123456;
+        double te2 = 123456.128456;
+        double add = NumberUtil.add(te1, te2);
+        System.out.println(add);
+        NumberUtil.round(te1, 4);//结果:123456.12
+        NumberUtil.round(te2, 4);//结果:123456.13
+
+        String s = NumberUtil.roundStr("23244.353434", 3);
+        System.out.println(s);
+
+        long c = 299792458;//光速
+        String format = NumberUtil.decimalFormat(",###", c);//299,792,458
+        System.out.println(format);
+
+//        NumberUtil.isNumber //是否为数字
+//        NumberUtil.isInteger //是否为整数
+//        NumberUtil.isDouble //是否为浮点数
+//        NumberUtil.isPrimes //是否为质数
+
+        int[] ints = NumberUtil.generateRandomNumber(12, 3000, 10);
+        Integer[] ints2 = NumberUtil.generateBySet(12, 3000, 10);
+        System.out.println(ints);
+
+        String s1 = NumberUtil.toStr(123.0023);
+        System.out.println(s1);
+
+        String binaryStr = NumberUtil.getBinaryStr(10);
+        System.out.println(binaryStr);
+
+        int xx = NumberUtil.binaryToInt(binaryStr);
+        long ll = NumberUtil.binaryToLong (binaryStr);
+        System.out.println(xx);
+        System.out.println(ll);
+
+        int compare = NumberUtil.compare(12.34, 12.44);
+    }
+
+    private static void testRuntimeUtil() {
+        String str = RuntimeUtil.execForStr("ipconfig");
+        System.out.println(str);
+    }
+
+    private static void testReflectUtil() {
+        Method[] methods = ReflectUtil.getMethods(Test3Hutool.class);
+
+        Method method = ReflectUtil.getMethod(Test3Hutool.class, "getId");
+        Test3Hutool test3Hutool = ReflectUtil.newInstance(Test3Hutool.class);
+        ReflectUtil.invoke(test3Hutool, "setA", 10);
+    }
+
+    private static void testUrlUtil() {
+        String url = "http://www.hutool.cn//aaa/bbb";
+        // 结果为：http://www.hutool.cn/aaa/bbb
+        String normalize = URLUtil.normalize(url);
+        System.out.println(normalize);
+
+        url = "http://www.hutool.cn//aaa/\\bbb?a=1&b=2";
+        // 结果为：http://www.hutool.cn/aaa/bbb?a=1&b=2
+        normalize = URLUtil.normalize(url);
+        System.out.println(normalize);
+
+        String body = "366466 - 副本.jpg";
+        // 结果为：366466%20-%20%E5%89%AF%E6%9C%AC.jpg
+        String encode = URLUtil.encode(body);
+        System.out.println(encode);
+
+//        URLUtil.toURI();
+//        ObjectUtil.isNull()
+    }
+
+    private static void testIoUtil() {
+        BufferedInputStream in = FileUtil.getInputStream("d:/test.txt");
+        BufferedOutputStream out = FileUtil.getOutputStream("d:/test2.txt");
+        long copySize = IoUtil.copy(in, out, IoUtil.DEFAULT_BUFFER_SIZE);
+
+
     }
 
     private static void testUuid() {
@@ -63,13 +173,16 @@ public class Test3Hutool {
     }
 
     private static void testStrUtil() {
-        byte[] aaas = StrUtil.bytes("aaa", "utf-8");
-        byte[] bytes = "aaa".getBytes();
-        System.out.println(aaas.length + "  " + bytes.length);
+        byte[] abyte = StrUtil.bytes("就像老鼠爱大米", "utf-8");
+        byte[] bbyte = "就像老鼠爱大米".getBytes();
+        System.out.println(abyte.length + "  " + bbyte.length);
+        System.out.println(new String(abyte) + " ---" + new String(bbyte) + " -- " + new String("就像老鼠爱大米".getBytes()));
 
         String template = "{}爱{}，就像老鼠爱大米";
         String str = StrUtil.format(template, "我", "你"); //str -> 我爱你，就像老鼠爱大米
         System.out.println(str);
+
+
     }
 
     private static void testFile() {
