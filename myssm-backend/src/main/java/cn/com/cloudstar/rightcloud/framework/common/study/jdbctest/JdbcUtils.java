843a01e4116ac6a53340db2d11d613249c3476e4
wuhong <wuhong@cloud-star.com.cn>
2019-11-04 00:18:30 +0800

    feat(*): 测试-字段类型longblob

--- myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/common/study/jdbctest/JdbcUtils.java
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/common/study/jdbctest/JdbcUtils.java
@@ -12,64 +12,63 @@ import cn.com.cloudstar.rightcloud.framework.common.util.DBCanst;
  * Jdbc工具类
  */
 public final class JdbcUtils {
-	
-	private static String url = DBCanst.url;
-	private static String username = DBCanst.user;
-	private static String password = DBCanst.password;
-	
-	/**
-	 * 构造器私用,防止直接创建对象,
-	 * 当然通过反射可以创建
-	 */
-	private JdbcUtils(){
-		
-	}
-	
-	//保证只是注册一次驱动
-	static{
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new ExceptionInInitializerError(e);
-		}
-	}
-	
-	/**
-	 * 获取连接
-	 * @return
-	 * @throws SQLException
-	 */
-	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(url, username, password);
-	}
-	
-	/**
-	 * 释放资源
-	 */
-	public static void free(ResultSet rs, Statement st, Connection conn) {
-		//规范的关系连接的方式
-		try{
-			if(rs != null) {
-				rs.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally{
-			try{
-				if(st != null) {
-					st.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}finally {
-				if(conn != null) {
-					try {
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
+
+    private static String url = DBCanst.url;
+    private static String username = DBCanst.user;
+    private static String password = DBCanst.password;
+
+    /**
+     * 构造器私用,防止直接创建对象, 当然通过反射可以创建
+     */
+    private JdbcUtils() {
+
+    }
+
+    //保证只是注册一次驱动
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    /**
+     * 获取连接
+     *
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    /**
+     * 释放资源
+     */
+    public static void free(ResultSet rs, Statement st, Connection conn) {
+        //规范的关系连接的方式
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (conn != null) {
+                    try {
+                        conn.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 }
--- myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/testtreeutil/Test5DbUtil.java
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/testtreeutil/Test5DbUtil.java
@@ -1,30 +1,80 @@
 package cn.com.cloudstar.rightcloud.framework.testtreeutil;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 import lombok.Data;
 
+import cn.com.cloudstar.rightcloud.framework.common.study.jdbctest.JdbcUtils;
 import cn.com.cloudstar.rightcloud.framework.common.util.DBUtil;
 
 public class Test5DbUtil {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+//        testApacheDbUtil();
+        testLongBlob();
+
+
+    }
+
+
+    private static void testLongBlob() throws Exception {
+        /**
+         CREATE TABLE person ( person_id INT auto_increment PRIMARY KEY,
+         first_name CHAR ( 45 ) NOT NULL,
+         second_name CHAR ( 45 ) NOT NULL,
+         photo LONGBLOB )
+         */
+
+        File file = new File("E:/testfiles/1-yes.jpg");
+        InputStream input = new BufferedInputStream(new FileInputStream(file));
+        String sql = "insert  into person(first_name, second_name, photo) values(?, ?, ?)";
+        Connection connection = JdbcUtils.getConnection();
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, "梁");
+        ps.setString(2, "志明");
+        ps.setBlob(3, input);
+        ps.executeUpdate();
+        System.out.println("增加成功！");
+
+        PreparedStatement pstmt = connection.prepareStatement("select * from person");
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+            InputStream is = rs.getBinaryStream("photo");
+            FileOutputStream fos = new FileOutputStream("e:/temp/photo" + rs.getInt("person_id") + ".jpg");
+            byte[] buffer = new byte[4096];
+            while ((is.read(buffer)) != -1) {
+                fos.write(buffer);
+            }
+            fos.close();
+        }
+        System.out.println("生成附件成功！");
+
+        JdbcUtils.free(rs, ps, connection);
+    }
+
+    private static void testApacheDbUtil() {
         String sql = "select * from sys_m_user where user_sid = ?";
-
-        List<TestSysUser> maps = DBUtil.queryBeanList(sql, TestSysUser.class, 100);
-
-        for (TestSysUser vo : maps) {
+        List<TestSysUser> beanList = DBUtil.queryBeanList(sql, TestSysUser.class, 100);
+        for (TestSysUser vo : beanList) {
             System.out.println(vo);
         }
     }
 
     @Data
-    public static class TestSysUser{
+    public static class TestSysUser {
+
         private Long user_sid;
         private String account;
-
     }
-
-
 
 }
