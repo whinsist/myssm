4e3961b6f93345834746d884c5bd9558408cdfef
wuhong <wuhong@cloud-star.com.cn>
2019-11-18 16:18:53 +0800

    feat(*): 测试mybatis拦截器

--- myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/common/mybatis/interceptor/ResultInterceptor.java
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/common/mybatis/interceptor/ResultInterceptor.java
@@ -1,14 +1,13 @@
 package cn.com.cloudstar.rightcloud.framework.common.mybatis.interceptor;
 
 
-import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.resultset.ResultSetHandler;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.plugin.*;
-import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.session.RowBounds;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -22,18 +21,16 @@ import cn.com.cloudstar.rightcloud.framework.common.mybatis.interceptor.helper.P
 import cn.com.cloudstar.rightcloud.framework.common.mybatis.interceptor.helper.PermissionUtils;
 
 /**
- * mybatis数据权限拦截器 - handleResultSets
- * 对结果集进行过滤
+ * mybatis数据权限拦截器 - handleResultSets 对结果集进行过滤
+ *
  * @author GaoYuan
  * @date 2018/4/17 上午9:52
  */
 @Intercepts({
-        @Signature(type = ResultSetHandler.class, method = "handleResultSets", args={Statement.class})
+        @Signature(type = ResultSetHandler.class, method = "handleResultSets", args = {Statement.class})
 })
 @Component
 public class ResultInterceptor implements Interceptor {
-    /** 日志 */
-    private static final Logger log = LoggerFactory.getLogger(ResultInterceptor.class);
 
     @Override
     public Object plugin(Object target) {
@@ -41,53 +38,32 @@ public class ResultInterceptor implements Interceptor {
     }
 
     @Override
-    public void setProperties(Properties properties) {}
+    public void setProperties(Properties properties) {
+    }
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        if(log.isInfoEnabled()){
-            log.info("进入 ResultInterceptor 拦截器...");
-        }
-
-//        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
-//        Object[] args = invocation.getArgs();
-//        MappedStatement ms = (MappedStatement) args[0];
-//        Object parameter = args[1];
-//        RowBounds rowBounds = (RowBounds) args[2];
-//        ResultHandler resultHandler = (ResultHandler) args[3];
-
-        //DefaultResultSetHandler handler = (DefaultResultSetHandler) invocation.getTarget();
-        //ParameterHandler parameterHandler = (ParameterHandler)ReflectUtil.getFieldValue(handler, "parameterHandler");
-        //Object parameterObj = parameterHandler.getParameterObject();
-
         ResultSetHandler resultSetHandler1 = (ResultSetHandler) invocation.getTarget();
         //通过java反射获得mappedStatement属性值
         //可以获得mybatis里的resultype
-        MappedStatement mappedStatement = (MappedStatement) ReflectUtil.getFieldValue(resultSetHandler1, "mappedStatement");
+        MappedStatement mappedStatement = (MappedStatement) ReflectUtil.getFieldValue(resultSetHandler1,
+                                                                                      "mappedStatement");
         //获取切面对象
         PermissionAop permissionAop = PermissionUtils.getPermissionByDelegate(mappedStatement);
 
         //执行请求方法，并将所得结果保存到result中
         Object result = invocation.proceed();
-        if(permissionAop != null) {
-            if (result instanceof ArrayList) {
-                ArrayList resultList = (ArrayList) result;
-                for (int i = 0; i < resultList.size(); i++) {
-                    Object oi = resultList.get(i);
-                    Class c = oi.getClass();
-                    Class[] types = {String.class};
-                    Method method = c.getMethod("setRegionCd", types);
-                    // 调用obj对象的 method 方法
-                    method.invoke(oi, "newvalue");
-                    if(log.isInfoEnabled()){
-                        log.info("数据权限处理【过滤结果】...");
-                    }
-                }
+        if (permissionAop != null && result instanceof ArrayList) {
+            ArrayList resultList = (ArrayList) result;
+            for (int i = 0; i < resultList.size(); i++) {
+                Object item = resultList.get(i);
+                Method method = item.getClass().getMethod("setRegionCd", String.class);
+                // 调用obj对象的 method 方法
+                method.invoke(item, "新值");
             }
         }
         return result;
     }
-
 
 
 }
