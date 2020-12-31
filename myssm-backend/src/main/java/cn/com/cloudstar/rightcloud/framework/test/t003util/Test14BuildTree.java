d9f8e1997ba4c61ea82b46b98c48e98151e3fb47
wuhong <444089024@qq.com>
2020-12-31 09:25:23 +0800

    测试构建tree

--- myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/test/t003util/Test14BuildTree.java
+++ myssm-backend/src/main/java/cn/com/cloudstar/rightcloud/framework/test/t003util/Test14BuildTree.java
@@ -1,24 +1,23 @@
 package cn.com.cloudstar.rightcloud.framework.test.t003util;
 
+import cn.hutool.core.io.FileUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Test14BuildTree {
-    private static final int ROOTID_IS_ZORE = 0;
+
     public static JSONArray treeRecursionDataList(List<Map<String, Object>> treeList, int parentId) {
         JSONArray childMenu = new JSONArray();
         for (Object object : treeList) {
             JSONObject jsonMenu = JSONObject.parseObject(JSON.toJSONString(object));
-            int menuId = jsonMenu.getIntValue("id");
-            int pid = jsonMenu.getIntValue("parentId");
-            if (parentId == pid) {
-                JSONArray nodes = treeRecursionDataList(treeList, menuId);
+            int keyValue = jsonMenu.getIntValue("id");
+            int parentValue = jsonMenu.getIntValue("parentId");
+            if (Objects.equals(parentId, parentValue)) {
+                JSONArray nodes = treeRecursionDataList(treeList, keyValue);
                 jsonMenu.put("childs", nodes);
                 childMenu.add(jsonMenu);
             }
@@ -27,18 +26,51 @@ public class Test14BuildTree {
     }
 
 
+    public static JSONArray treeRecursionCategory(List<CategoryPO> list, Long parentId) {
+        JSONArray childMenu = new JSONArray();
+        for (Object object : list) {
+            JSONObject jsonMenu = JSONObject.parseObject(JSON.toJSONString(object));
+            Long keyValue = jsonMenu.getLong("id");
+            Long parentValue = jsonMenu.getLong("parentId");
+            if (Objects.equals(parentId, parentValue)) {
+                JSONArray nodes = treeRecursionCategory(list, keyValue);
+                jsonMenu.put("childs", nodes);
+                childMenu.add(jsonMenu);
+            }
+        }
+        return childMenu;
+    }
+
+
+
+
     public static void main(String[] args) {
         // 获得用户岗位包含的所有分类
         List<Map<String, Object>> userJobCategoryList = getUserJobContainAllCategoryList();
-        JSONArray returnList = treeRecursionDataList(userJobCategoryList, ROOTID_IS_ZORE);
-        System.out.println(JSON.toJSONString(returnList));
+        JSONArray treeResult1 = treeRecursionDataList(userJobCategoryList, 0);
+        System.out.println(JSON.toJSONString(treeResult1));
+
+
+        //
+        List<CategoryPO> list2 = getList2();
+        JSONArray treeResult2 = treeRecursionCategory(list2, 0L);
+        // parentId的值决定了从哪个根节点开始
+        //JSONArray treeResult2 = treeRecursionCategory(list2, 2012300000004187L);
+        System.out.println(JSON.toJSONString(treeResult2));
+
     }
+
+    private static List<CategoryPO> getList2() {
+        String str = FileUtil.readUtf8String("d:/tmp/list.txt");
+        return JSON.parseArray(str, CategoryPO.class);
+    }
+
 
     public static List<Map<String, Object>> getUserJobContainAllCategoryList() {
         List<Map<String, Object>> list = new ArrayList<>();
         Map<String, Object> map1 = new HashMap<>();
         map1.put("id", 1);
-        map1.put("parentId", ROOTID_IS_ZORE);
+        map1.put("parentId", 0);
         map1.put("name", "岗位资格性培训");
         list.add(map1);
         Map<String, Object> map2 = new HashMap<>();
@@ -49,7 +81,7 @@ public class Test14BuildTree {
 
         Map<String, Object> map3 = new HashMap<>();
         map3.put("id", 3);
-        map3.put("parentId", ROOTID_IS_ZORE);
+        map3.put("parentId", 0);
         map3.put("name", "test3");
         list.add(map3);
         Map<String, Object> map4 = new HashMap<>();
@@ -63,8 +95,16 @@ public class Test14BuildTree {
         map5.put("parentId", 666);
         map5.put("name", "test5");
         list.add(map5);
-
         return list;
+    }
+
+
+    @Data
+    public static class CategoryPO {
+        private Long id;
+        private String name;
+        private Integer level;
+        private Long parentId;
     }
 }
 
